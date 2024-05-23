9dd1fc0e09b2a8ac5a18961a7718023dca265dd2
Kasper Korthsen <Kalletheman@live.dk>
2024-05-23 21:16:58 +0200

    Rettet på kodens struktur
    Ændret så den bruger e.file til kun at køre når den er null, så den kan fyldes ud af den submitform til nye brugere der er påbegyndt

--- Blazor/Models/users.cs
+++ Blazor/Models/users.cs
@@ -8,11 +8,15 @@ namespace Blazor.Models
 		[Key]
 		public int Id { get; set; }
 
-        public string? Name { get; set; }
+        public string UserName { get; set; } = "Test";
 
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string? FileName { get; set; }
 
         public string? Email { get; set; }
-
 
         public string? Password { get; set; }
 
