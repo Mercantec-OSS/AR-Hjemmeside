7b0a288bf7a08cf1fd91900487a0b7562b5020ce
Syzygy622f <79576617+Syzygy622f@users.noreply.github.com>
2024-10-02 14:04:29 +0200

    tilføjelse af api kald til frontend

---
+++ Blazor/Migrations/20241002062759_InitialCreate.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blazor.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateTable(
+                name: "ThreeDModels",
+                columns: table => new
+                {
+                    object_name = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    subject_id = table.Column<int>(type: "int", nullable: false),
+                    FK_Users_Id = table.Column<int>(type: "int", nullable: false),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    datetime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    hidden = table.Column<bool>(type: "bit", nullable: false),
+                    tags = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    user_picture = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    qr_code = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    file = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    category = table.Column<int>(type: "int", nullable: false),
+                    copyright = table.Column<int>(type: "int", nullable: false),
+                    copyright_info = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ThreeDModels", x => x.object_name);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ThreeDModels");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
